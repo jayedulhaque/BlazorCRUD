722d81eb791090309c74334c512d285347fcf450
jayedul haque <jayedulhaque15@gmail.com>
2023-08-31 17:37:31 +0600

    This is a new commit for what I originally planned to be an amendmend

---
+++ BlazorTEst/Server/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazorTEst.Shared;
+using Microsoft.EntityFrameworkCore;
+using BlazorTEst.Shared.Models;
+
+namespace BlazorTEst.Server.Data
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<User> Users { get; set; } = null!;
+    }
+}
